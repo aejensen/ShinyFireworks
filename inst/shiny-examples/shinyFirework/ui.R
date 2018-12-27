f654a663d0916d1a94349440be2d63b215995e45
Andreas Jensen <aejensen@users.noreply.github.com>
2018-12-28 00:16:28 +0100

    Fixing visual design

--- inst/shiny-examples/shinyFirework/ui.R
+++ inst/shiny-examples/shinyFirework/ui.R
@@ -1,3 +1,5 @@
-ui <- shiny::bootstrapPage(
-  ggvis::ggvisOutput("fplot")
+ui <- shiny::fluidPage(
+  shinyWidgets::setBackgroundColor(color = "ghostwhite", gradient = c("linear", "radial"), direction = c("bottom", "top", "right", "left")),
+  h1("Happy New Year 2018!", align = "center"),
+  column(12, align="center", ggvis::ggvisOutput("fplot"))
 )
