b4a2e6f4032b8b4e426db72c3838d50bcbf067cc
Andreas Jensen <aejensen@users.noreply.github.com>
2019-11-27 20:13:27 +0100

    Update ui.R

--- inst/shiny-examples/shinyFirework/ui.R
+++ inst/shiny-examples/shinyFirework/ui.R
@@ -1,5 +1,5 @@
 ui <- shiny::fluidPage(
   shinyWidgets::setBackgroundColor(color = "ghostwhite", gradient = c("linear", "radial"), direction = c("bottom", "top", "right", "left")),
-  h1("Happy New Year 2019!", align = "center"),
-  column(12, align="center", ggvis::ggvisOutput("fplot"))
+  shiny::h1("Happy New Year 2019!", align = "center"),
+  shiny::column(12, align="center", ggvis::ggvisOutput("fplot"))
 )
